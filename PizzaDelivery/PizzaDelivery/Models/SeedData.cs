87c513b9696a8c8e2c39c1d52af5414694a0532a
taldarim3 <taldarim3@gmail.com>
2021-11-23 18:01:49 +0300

    Добавил инициализацию AdditionalProducts

--- PizzaDelivery/PizzaDelivery/Models/SeedData.cs
+++ PizzaDelivery/PizzaDelivery/Models/SeedData.cs
@@ -26,8 +26,7 @@ public static class SeedData
                 Name = "Пепперони",
                 Cost = 349,
                 Weight = 520,
-                Description = @"Пикантная пепперони, увеличенная порция моцареллы, 
-                                            томаты, томатный соус"
+                Description = "Пикантная пепперони, увеличенная порция моцареллы, томаты, томатный соус"
             },
             new Pizza()
             {
@@ -35,8 +34,7 @@ public static class SeedData
                 Name = "Маргарита",
                 Cost = 369,
                 Weight = 620,
-                Description = @"Увеличенная порция моцареллы, томаты,
-                                          итальянские травы, томатный соус"
+                Description = "Увеличенная порция моцареллы, томаты, итальянские травы, томатный соус"
             },
             new Pizza()
             {
@@ -68,8 +66,7 @@ public static class SeedData
                 Name = "Мясная",
                 Cost = 779,
                 Weight = 640,
-                Description = @"Пикантная пепперони, цыпленок, острая чоризо, 
-                                       бекон, митболы, моцарелла, томатный соус"
+                Description = "Пикантная пепперони, цыпленок, острая чоризо, бекон, митболы, моцарелла, томатный соус"
             },
             new Pizza()
             {
@@ -77,8 +74,8 @@ public static class SeedData
                 Name = "Грибная",
                 Cost = 399,
                 Weight = 680,
-                Description = @"Шампиньоны, томаты, сладкий перец, красный лук, маслины, кубики брынзы,
-                                         моцарелла, томатный соус, итальянские травы"
+                Description =
+                    "Шампиньоны, томаты, сладкий перец, красный лук, маслины, кубики брынзы, моцарелла, томатный соус, итальянские травы"
             },
             new Pizza()
             {
@@ -86,10 +83,71 @@ public static class SeedData
                 Name = "Четыре сезона",
                 Cost = 399,
                 Weight = 680,
-                Description = @"Увеличенная порция моцареллы, ветчина, пикантная пепперони,
-                                        кубики брынзы, томаты, шампиньоны,
-                                        итальянские травы, томатный соус"
+                Description =
+                    "Увеличенная порция моцареллы, ветчина, пикантная пепперони, кубики брынзы, томаты, шампиньоны, итальянские травы, томатный соус"
             });
+            if (PizzaDeliveryDbContext == null || PizzaDeliveryDbContext.AdditionalProducts == null)
+        {
+            throw new ArgumentNullException("Null PizzaDeliveryDbContext");
+        }
+
+        if (PizzaDeliveryDbContext.AdditionalProducts.Any())
+        {
+            return; // DB has been seeded
+        }
+
+        PizzaDeliveryDbContext.AdditionalProducts.AddRange(
+            new AdditionalProduct()
+            {
+                Id = 1,
+                Name = "Coca-Cola",
+                Cost = 69
+            },
+             new AdditionalProduct(){
+                 Id =2,
+                 Name = "Sprite",
+                 Cost = 69
+            },
+            new AdditionalProduct(){
+                Id =3,
+                Name = "Fanta",
+                Cost = 69
+            },
+            new AdditionalProduct(){
+                Id =4,
+                Name = "Lipton",
+                Cost = 79
+            },
+            new AdditionalProduct(){
+                Id =5,
+                Name = "Orbit",
+                Cost = 39
+            },
+            new AdditionalProduct(){
+                Id =6,
+                Name = "Влажная салфетка",
+                Cost = 5
+            },
+            new AdditionalProduct(){
+                Id =7,
+                Name = "Фирменный магнит",
+                Cost = 99
+            },
+            new AdditionalProduct(){
+                Id =8,
+                Name = "Морс",
+                Cost = 49
+            },
+            new AdditionalProduct(){
+                Id =9,
+                Name = "Сок апельсиновый",
+                Cost = 79
+            },
+            new AdditionalProduct(){
+                Id = 10,
+                Name = "Сок яблочный",
+                Cost = 79
+            });;
         PizzaDeliveryDbContext.SaveChanges(); //?
     }
 }
